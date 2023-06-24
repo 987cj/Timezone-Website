1b89d312bc4dbc3ff1a1236ca3b8c9048b5052d8
987cj <cjoycemartyr@gmail.com>
2023-06-25 00:13:15 +0930

    selection now stays even if innerHTML changed - also more timezones added

--- zonedisplay.js
+++ zonedisplay.js
@@ -11,13 +11,17 @@ class ZonePanel {
 		let disp_string = "";
 		disp_string += "<div class=\"zonepanel\" id=\"" + this.id + "\">";
 		disp_string += "<br>";
-		disp_string += "<form onchange=\"changeZone(this);\">";
-		disp_string += "<h4> Timezone:</h4>";
-		disp_string += "<select class=\"zones\">";
-		disp_string += "<option value=\"ACST\">ACST</option>";
+		disp_string += "<h4>Timezone:</h4>";
+		disp_string += "<select type=\"select\" class=\"zones\" onchange=\"changeZone(this);\">";
+		disp_string += "<option value=\"ACDT\">ACDT +10:30</option>";
+		disp_string += "<option value=\"ACST\">ACST +09:30</option>";
+		disp_string += "<option value=\"ACT\">ACT -05:00</option>";
+		disp_string += "<option value=\"ADT\">ADT -03:00</option>";
+		disp_string += "<option value=\"AEDT\">AEDT +11:00</option>";
+		disp_string += "<option value=\"ACT\">ACT</option>";
 		disp_string += "<option value=\"GMT\" selected>GMT</option>";
-		disp_string += "</select></form>";
-		disp_string += "<h2 class=\"timer\">Time</h2>";
+		disp_string += "</select>";
+		disp_string += "<h2 class=\"timer\"></h2>";
 		disp_string += "</div>";
 
 		zone.innerHTML += disp_string;
@@ -30,8 +34,20 @@ class ZonePanel {
 			case "GMT":
 				this.time_add = 0;
 				break;
+			case "ACDT":
+				this.time_add = 10.5;
+				break;
 			case "ACST":
 				this.time_add = 9.5;
+				break;
+			case "ACT":
+				this.time_add = -5;
+				break;
+			case "ADT":
+				this.time_add = -3;
+				break;
+			case "AEDT":
+				this.time_add = 11;
 				break;
 			default:
 				this.time_add = 0;
